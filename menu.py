7d4048ae324a6fb6f3448ae15dac4bc7452d0794
Danna <Cb500889@gmail.com>
2025-09-03 19:42:54 -0500

    suma y resta calculadora

--- menu.py
+++ menu.py
@@ -1,23 +1,15 @@
-def suma (num1 ,num2):
+def suma(num1, num2):
     return num1 + num2
 
-def main ():
-    print(suma)
-
-def resta (num1 ,num2):
+def resta(num1, num2):
     return num1 - num2
 
-def main ():
-    print(resta)
+elif opcion == "1":
+    n1 = int(input("Número 1: "))
+    n2 = int(input("Número 2: "))
+    print("Resultado:", suma(n1, n2))
 
-
-
-num1 = int(input("Ingrese el primer numero: "))
-num2 = int(input("Ingrese el segundo numero: "))
-
-
-resultado = suma(num1, num2)
-print("El resultado de la suma es:", resultado)
-
-resultado = resta(num1, num2)
-print("El resultado de la resta es:", resultado)
+elif opcion == "2":
+    n1 = int(input("Número 1: "))
+    n2 = int(input("Número 2: "))
+    print("Resultado:", resta(n1, n2))
