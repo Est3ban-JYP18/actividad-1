a81153751d6b87d0e9ec3d869ee68a3660aa3d3a
unknown <andresromerovr@gmail.com>
2025-09-03 20:53:24 -0500

    multiplicacion y division

--- menu.py
+++ menu.py
@@ -6,11 +6,11 @@ def division(num1, num2):
  return num1 / num2
 
 elif opcion == "3":
-n1 = int(input("Número 1: "))
-n2 = int(input("Número 2: "))
-print("Resultado:", multiplicacion(n1, n2))
+ n1 = int(input("Número 1: "))
+ n2 = int(input("Número 2: "))
+ print("Resultado:", multiplicacion(n1, n2))
 
 elif opcion == "4":
-n1 = int(input("Número 1: "))
-n2 = int(input("Número 2: "))
-print("Resultado:", division(n1, n2))
+ n1 = int(input("Número 1: "))
+ n2 = int(input("Número 2: "))
+ print("Resultado:", division(n1, n2))
